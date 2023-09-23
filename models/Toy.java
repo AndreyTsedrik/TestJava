67098080f0d696939b589561a2b07ab89cb3732f
Andrey <awesome.foxe@yandex.ru>
2023-09-23 19:08:38 +0300

    add Toy

---
+++ models/Toy.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Toy {
+    public int id;
+    public String name;
+    public int weight;
+
+    public Toy(int toyId, String toyName, int toyWeight) {
+        id = toyId;
+        name = toyName;
+        weight = toyWeight;
+    }
+}
